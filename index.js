456920e06b203a239d005944ae92942e92c349ad
Susan Tan <susan.tan198@gmail.com>
2019-01-28 20:04:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Susan"
-var height = "74"
+var height = 74;
 
 console.log("Name:")
 console.log(name)
 
-console.log("Height:" + 1);
+console.log(parseInt("Height:" + 1));
 console.log(height)
